5f7f8c396776a8daad83c2c9c4162747ed5489a6
JuliaOtt <kindergabel@gmail.com>
2016-01-15 13:47:32 +0100

    added a model for Orders associated Orders Model with Users and Products belongs_to and has_many

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,3 @@
-class Product < ActiveRecord::Base
+class Product < ActiveRecord::Base # This declaration means that your product class is a subclass of, or inherits from, the ActiveRecord::Base class, which is the Rails library that provides logic for the model. 
+	has_many :orders
 end
