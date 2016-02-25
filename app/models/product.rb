ae7880bb62f0855a70d0632b34a3bd212be69416
JuliaOtt <kindergabel@gmail.com>
2016-02-25 10:44:52 +0700

    added comment_model and styled the product show page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ActiveRecord::Base # This declaration means that your product class is a subclass of, or inherits from, the ActiveRecord::Base class, which is the Rails library that provides logic for the model. 
 	has_many :orders
+	has_many :comments
 end
