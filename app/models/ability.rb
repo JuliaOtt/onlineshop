6ca0f3de47c9d1725e65844a58b1ae6538c64f7e
JuliaOtt <kindergabel@gmail.com>
2016-03-03 12:30:46 +0700

    added admin flag and delete function

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,10 +5,14 @@ class Ability
     # Define abilities for the passed in user here. For example:
     #
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
-    #   else
-    #     can :read, :all
-    #   end
+    if user.admin?
+        can :manage, :all
+    else
+        can :manage, User, id: user.id
+        can :destroy, Comment, :user_id => user.id
+        can :read, :all 
+        can [:create, :update] , [Product, Comment]
+    end
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
