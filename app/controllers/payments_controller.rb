11236df8cccbff413cd520dcc22f4078ae18a6b1
JuliaOtt <kindergabel@gmail.com>
2016-03-20 13:43:51 +0700

    added paymentsController and changed the schema price field

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,28 @@
+class PaymentsController < ApplicationController
+	before_filter :authenticate_user!
+
+	def create
+		token = params[:stripeToken]
+		@product = Product.find(params[:product_id])
+		@user = current_user
+
+		begin
+			charge = Stripe::Charge.create(
+				amount: @product.price_in_cents,
+				currency: 'eur',
+				source: token,
+				description: @product.name
+			)
+
+			if charge.paid
+				Order.create(
+					product: @product,
+					user: @user,
+					total: @product.price
+				)
+			end
+		rescue Stripe::CardError => e
+			# The card has been declined
+		end
+	end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160302042138) do
+ActiveRecord::Schema.define(version: 20160317033653) do
 
   create_table "average_caches", force: :cascade do |t|
     t.integer  "rater_id"
@@ -55,10 +55,10 @@ ActiveRecord::Schema.define(version: 20160302042138) do
     t.string   "name"
     t.text     "description"
     t.string   "image_url"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
     t.string   "colour"
-    t.decimal  "price", :precision => 2
+    t.decimal  "price",       precision: 15, scale: 2
   end
 
   create_table "rates", force: :cascade do |t|
